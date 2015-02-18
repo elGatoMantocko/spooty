110f4cca1df96a70b176a9bdeb763d504d75ec2b
Elliott Mantock <emantock@gmail.com>
2015-02-17 21:45:58 -0500

    started working on position timer

--- index.js
+++ index.js
@@ -133,6 +133,13 @@ app.get('/downvote', function(req, res) {
   io.emit('vote_tally', votes, false);
 });
 
+setTimeout(function() {
+  mpdclient.sendCommand('status', []),
+  var position
+  //console.log(position);
+  io.emit('position', position)
+}, 1000);
+
 mpdclient.on('system-player', function() {
   mpdclient.sendCommand(cmd('currentsong', []), function(err, data) {
     if (err) throw err;
